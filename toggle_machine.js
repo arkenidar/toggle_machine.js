0ae158bdcb8499353f2c2d6c5031f3070560065b
Dario Cangialosi <dario.cangialosi@gmail.com>
2018-02-12 20:02:37 +0100

    added detection of 'infinite looping', with examples.

--- toggle_machine.js
+++ toggle_machine.js
@@ -7,11 +7,35 @@ example_program=[
 ['T', 0, 'end', 0],
 ]
 
-run(example_program)
+example_program_looping=[
+['O', 1, 1],
+['O', 0, 2],
+['T', 0, 0, 3],
+['T', 0, 0, 0],
+]
+
+example_program_looping_simple=[
+['O', 1, 1],
+['T', 0, 1, 0],
+]
+
+run(example_program_looping_simple)
 
 function run(program){
     var current_step_index=0
+    var history=[]
     while(current_step_index!='end'){
+
+        // detection of "infinite looping"
+        var snapshot=JSON.stringify([current_step_index, program])
+        if(history.indexOf(snapshot)==-1){
+            history.push(snapshot)
+        }else{
+            console.log('infinite looping was detected')
+            return
+        }
+
+        // execute a step of the program
         var step=program[current_step_index]
         if(step[0]=='T'){
             // toggle
